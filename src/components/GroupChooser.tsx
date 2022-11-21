bd07702caba13dce81a51e6ecbc31fa09281b414
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2022-11-21 14:10:01 +0200

    Open link in new window

--- src/components/GroupChooser.tsx
+++ src/components/GroupChooser.tsx
@@ -1,96 +1,3 @@
-// import * as React from 'react';
-// import Button from '@mui/material/Button';
-// import Menu from '@mui/material/Menu';
-// import MenuItem from '@mui/material/MenuItem';
-// import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-// import {Stack, Typography} from "@mui/material";
-// import {
-//     AllGroupsDominatorSchedule,
-//     AllGroupsNumeratorSchedule,
-//     NumeratorLessonsSchedule_KN_31_2
-// } from "../data/lessonsSchedule/lessonsSchedule";
-// import {useState} from "react";
-// import {IGroup} from "../interfaces";
-// import {useAppDispatch, useAppSelector} from "../hooks";
-// import {setGroupSchedule, setIsNumeratorWeek} from "../store/slices";
-// import {isEvenWeek} from "../usefulFunctions/usefulFunctions";
-//
-//
-// const GroupChooser = () => {
-//
-//     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-//
-//     const [selectedGroup, setSelectedGroup] = useState<IGroup>(NumeratorLessonsSchedule_KN_31_2);
-//
-//     const dispatch = useAppDispatch();
-//
-//     const {isNumerator} = useAppSelector(state => state.lessonReducer)
-//
-//     const open = Boolean(anchorEl);
-//     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-//         setAnchorEl(event.currentTarget);
-//     };
-//
-//     const handleClose = (group: IGroup) => {
-//
-//         if (!isNumerator){
-//             dispatch(setIsNumeratorWeek(true))
-//         }
-//
-//         setSelectedGroup(group)
-//         dispatch(setGroupSchedule(group))
-//
-//         setAnchorEl(null);
-//     };
-//
-//     return (
-//         <div>
-//             <Button
-//                 id="basic-button"
-//                 aria-controls={open ? 'basic-menu' : undefined}
-//                 aria-haspopup="true"
-//                 aria-expanded={open ? 'true' : undefined}
-//                 onClick={handleClick}
-//                 sx={{color: 'black', backgroundColor: 'rgb(239, 240, 248)', width: '200px'}}
-//             >
-//                 <Stack direction={'row'} justifyContent={'space-between'} width={'95%'}>
-//                     <Typography>
-//                         {selectedGroup?.group}
-//                     </Typography>
-//                     <KeyboardArrowDownIcon/>
-//                 </Stack>
-//             </Button>
-//             <Menu
-//                 id="basic-menu"
-//                 anchorEl={anchorEl}
-//                 open={open}
-//                 onClose={handleClose}
-//                 MenuListProps={{
-//                     'aria-labelledby': 'basic-button',
-//                 }}
-//             >
-//
-//                 {
-//                     !isEvenWeek() ?
-//                         AllGroupsNumeratorSchedule.map(grp => (
-//                             <MenuItem onClick={() => handleClose(grp)} sx={{width: '200px'}}
-//                                       key={grp.group}>{grp.group}</MenuItem>
-//                         ))
-//                         :
-//                         AllGroupsDominatorSchedule.map(grp => (
-//                             <MenuItem onClick={() => handleClose(grp)} sx={{width: '200px'}}
-//                                       key={grp.group}>{grp.group}</MenuItem>
-//                         ))
-//                 }
-//
-//             </Menu>
-//         </div>
-//     );
-// }
-//
-// export default GroupChooser;
-
-
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
@@ -109,7 +16,6 @@ import {useState} from "react";
 import {setGroupSchedule, setIsNumeratorWeek} from "../store/slices";
 
 const GroupChooser = () => {
-    const [selectedGroup, setSelectedGroup] = useState<IGroup>(NumeratorLessonsSchedule_KN_31_2);
     const [groupName, setGroupName] = React.useState(NumeratorLessonsSchedule_KN_31_2.group);
 
     const dispatch = useAppDispatch();
@@ -126,9 +32,7 @@ const GroupChooser = () => {
             dispatch(setIsNumeratorWeek(true))
         }
 
-        setSelectedGroup(group)
         dispatch(setGroupSchedule(group))
-
     };
 
 
